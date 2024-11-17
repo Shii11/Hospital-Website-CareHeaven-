48307dd7b75053f8f9a6d3a0d14427dcc5fceb55
Shii11 <priyakshisaikia117@gmail.com>
2024-11-17 21:02:52 +0530

    files added

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    document.getElementById('hamburger').addEventListener('click', function() {
+        const navbar = document.querySelector('.navbar');
+        navbar.classList.toggle('active'); // Toggle the 'active' class
+    });
